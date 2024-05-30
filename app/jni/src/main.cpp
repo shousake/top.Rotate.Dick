0e3f4963590dfd6fa78a5a5e021b82f5655074f3
shousake <154686982+shousake@users.noreply.github.com>
2024-05-30 19:42:11 +0800

    Update main.cpp

--- app/jni/src/main.cpp
+++ app/jni/src/main.cpp
@@ -1,25 +1,28 @@
 #include<SDL.h>
+#include<stdlib.h>
+#include<time.h>
 struct Dick
 {
     double deg;
-    SDL_Surface *orDick;
+    SDL_Surface* orDick;
     SDL_bool isDick;
-    SDL_Texture *Dtext;
+    SDL_Texture* Dtext;
     SDL_Rect DPos;
+    int RotateTime;
 };
+int rnum;
 struct Dick d[6];
 int main(int argc, char* argv[]) {
     // 初始化SDL
 
-    int logical_width = 970;
-    int logical_height = 545;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         // 处理初始化失败的情况
         return 1;
     }
-    int window_width, window_height;
-
-
+    srand((unsigned int)time(NULL));
+    int logical_width = 970;
+    int logical_height = 545;
     ///Users/shousake/Desktop/files/SDL/SDL/dick/
     d[0].orDick = SDL_LoadBMP("dicks/dickhead.bmp");
     d[1].orDick = SDL_LoadBMP("dicks/dickhead1.bmp");
@@ -31,15 +34,15 @@ int main(int argc, char* argv[]) {
     SDL_RenderSetLogicalSize(renderer, logical_width, logical_height);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
     // SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, dick);
-    SDL_Texture *Tbk =SDL_CreateTextureFromSurface(renderer, bk);
-    for (int i=0;i<4;i++)
-        d[i].isDick = SDL_FALSE;
-    for (int i=0;i<4;i++)
-        d[i].Dtext = SDL_CreateTextureFromSurface(renderer, d[i].orDick);
-    for (int i=0;i<4;i++)
-        d[i].deg = 0;
+    SDL_Texture* Tbk = SDL_CreateTextureFromSurface(renderer, bk);
+    int spcX = 970;
+    bool spcFirst=1;
     for (int i = 0; i < 4; i++) {
-        d[i].DPos.x = 100+i * 200;
+        d[i].isDick = SDL_FALSE;
+        d[i].Dtext = SDL_CreateTextureFromSurface(renderer, d[i].orDick);
+        d[i].deg = 0;
+        d[i].RotateTime = 0;
+        d[i].DPos.x = 100 + i * 200;
         d[i].DPos.y = 100;
         d[i].DPos.w = 78;
         d[i].DPos.h = 78;
@@ -50,14 +53,13 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
-    for(int i=0;i<4;i++)
+    for (int i = 0; i < 4; i++)
         SDL_FreeSurface(d[i].orDick);
     SDL_bool running = SDL_TRUE;
     SDL_bool showDick = SDL_FALSE;
 
-    SDL_Rect dick1 = {100,100,78,78};
+    SDL_Rect dick1 = { 100,100,78,78 };
 
-    int tmpDeg = 0;
     while (running)
     {
         SDL_Event event;
@@ -72,16 +74,16 @@ int main(int argc, char* argv[]) {
             }
             // 其它事件处理...
 
-            if (event.type == SDL_MOUSEBUTTONDOWN )
+            if (event.type == SDL_MOUSEBUTTONDOWN)
             {
                 //showDick = (SDL_bool)(showDick^1);
                 //event.button.x>10 && event.button.x<88&&event.button.y>100&&event.button.y<178
                 //&&event.tfinger.x<88&&event.tfinger.y>100&&event.tfinger.y<178)
 
-                for (int i=0;i<4;i++)
+                for (int i = 0; i < 4; i++)
                 {
 
-                    if(event.button.x> 100+i * 200&&event.button.x<178+i * 200&&event.button.y>100&&event.button.y<178)
+                    if (event.button.x > 100 + i * 200 && event.button.x < 178 + i * 200 && event.button.y>100 && event.button.y < 178)
                     {
                         d[i].isDick = SDL_TRUE;
                     }
@@ -94,30 +96,54 @@ int main(int argc, char* argv[]) {
 
         SDL_RenderClear(renderer);
         SDL_RenderCopyEx(renderer, Tbk, NULL, NULL, 0, NULL, SDL_FLIP_NONE);
-        for (int i=0;i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
             SDL_RenderCopyEx(renderer, d[i].Dtext, NULL, &d[i].DPos, d[i].deg, NULL, SDL_FLIP_NONE);
 
-            if(d[i].isDick == SDL_TRUE)
+            if (d[i].isDick == SDL_TRUE)
             {
-                d[i].deg+=3;
+                d[i].deg += 3;
 
             }
-            if (d[i].deg>=360)
+            if (d[i].deg >= 360)
             {
                 d[i].deg = 0;
                 d[i].isDick = SDL_FALSE;
+                d[i].RotateTime++;
             }
 
 
         }
+        if (d[0].RotateTime >= 4 && d[1].RotateTime >= 4 && d[2].RotateTime >= 4 && d[3].RotateTime >= 4)
+        {
+            if (spcFirst)
+            {
+                rnum = rand() % 4;
+                spcFirst = 0;
+            }
+
+            SDL_Rect r = {spcX,200 ,78,78};
+            SDL_RenderCopyEx(renderer, d[rnum].Dtext, NULL, &r, d[rnum].deg, NULL, SDL_FLIP_NONE);
+            spcX--;
+            if (spcX <= 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    d[i].RotateTime -= 4;
+
+                }
+                spcX = 970;
+                spcFirst = 1;
+            }
+        }
+
+
 
         SDL_RenderPresent(renderer); // 更新屏幕显示
         SDL_Delay(10);
         //SDL_UpdateWindowSurface(window);
         // 更新游戏状态和渲染
-        if (tmpDeg >= 360)
-            tmpDeg = 0;
+
 
 
     }
@@ -130,3 +156,4 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
